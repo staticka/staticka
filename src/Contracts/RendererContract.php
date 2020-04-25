a6ae389f938f6dc8ab9da06dc9dd81fcbfc25dc0
Rougin Gutib <rougingutib@gmail.com>
2020-04-25 20:59:50 +0800

    Modify RendererContract

--- src/Contracts/RendererContract.php
+++ src/Contracts/RendererContract.php
@@ -13,14 +13,4 @@ use Zapheus\Renderer\RendererInterface;
  */
 interface RendererContract extends RendererInterface
 {
-    /**
-     * Renders a file from a specified template.
-     *
-     * @param  string $template
-     * @param  array  $data
-     * @return string
-     *
-     * @throws \InvalidArgumentException
-     */
-    public function render($template, $data = array());
 }
